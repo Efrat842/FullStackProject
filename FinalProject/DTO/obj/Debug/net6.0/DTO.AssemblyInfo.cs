ecb0f4d73172622b49b2bdef8c7ac053342490fd
Efrat <e0556729842@gmail.com>
2026-07-05 00:37:29 +0300

    https://github.com/Efrat842/FullStackProject.git

--- FinalProject/DTO/obj/Debug/net6.0/DTO.AssemblyInfo.cs
+++ FinalProject/DTO/obj/Debug/net6.0/DTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021f5ff87fa58c928955d007459044b092ba935d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
